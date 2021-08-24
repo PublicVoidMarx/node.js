b54d2a43704acead94fc9e247795b941fbdc3fa4
Luciano criscuolo <luchigroso9@hotmail.com>
2021-08-24 13:56:33 -0300

    crud final

--- router/mascotas.js
+++ router/mascotas.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const Producto = require('../models/producto')
 
+
 router.get('/', async(req,res) => {
     try{
         const arrayProductosDB = await Producto.find()
@@ -24,7 +25,7 @@ router.post('/',async(req,res) => {
     const body = req.body
     try {
         await Producto.create(body)
-        res.redirect('/mascotas')
+        res.redirect('mascotas')
     } catch (error) {
         console.log(eror)
     }
@@ -42,9 +43,51 @@ router.get('/:id', async(req,res) => {
             error:true,
             mensaje:'Id doesnt exist'
         })
+    }   
+})
+
+router.delete('/:id', async(req,res) => {
+    const id = req.params.id
+    try {
+        const productoDB = await Producto.findByIdAndDelete({_id:id})
+        if(productoDB){
+            res.json({
+                estado:true,
+                mensaje:'eliminado'
+            })      
+        }else{
+            res.json({
+                estado:false,
+                mensaje:'ya se elimino o no existe'
+            })
+        }    
+    } catch (error) {
+        console.log(error)
     }
-    
-   
+})
+
+router.put('/:id', async(req,res) => {
+    const id = req.params.id
+    const body = req.body
+
+    //console.log('id:',id)
+    //console.log('body: ',body)
+
+    try {
+        const productoDB = await Producto.findByIdAndUpdate(
+            id,body,{useFindAndModify:false}
+        )
+        res.json({
+            estado: true,
+            mensaje: 'product updated'
+        })
+    } catch (error) {
+        console.log(error)
+        res.json({
+            estado: false,
+            mensaje: 'update failed'
+        })
+    }
 })
 
 module.exports = router
